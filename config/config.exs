3997be7aba35fe5d2245a2d106ea95c092b6f728
Selaelo0001 <selaelo.makgato@tauspace.com>
2023-08-16 23:26:53 +0200

    authenticate

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :real_deal_api, RealDealApiWeb.Auth.Guardian,
+  issuer: "real_deal_api",
+  secret_key: "PMqJO2KbEVn/DEXSYfOdmf6kQT3z3GFe8LQDuEjYlqVKXrzLfL6wWpd8eA6GdRpk"
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
